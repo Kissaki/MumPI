b34dd65bf5237bd0f100796c515ef3ee8984f202
Kissaki <kissaki@gmx.de>
2009-02-18 02:57:43 +0100

    style: initial main menu style lang en: structured file index template: welcome text added

--- languages/en.php
+++ languages/en.php
@@ -1,22 +1,20 @@
 <?php
 	
+	// Main Menu
 	$txt['home'] = 'Home';
 	$txt['login'] = 'Login';
+	$txt['register'] = 'Register';
 	$txt['logout'] = 'Log Out';
 	$txt['profile'] = 'Profile';
+	
+	// Account Information
 	$txt['email'] = 'email';
 	$txt['username'] = 'username';
 	$txt['password'] = 'password';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
-	$txt['email'] = 'email';
+	$txt['texture'] = 'texture';
+	$txt['userid'] = 'user id';
+	
+	
+	//$txt['email'] = 'email';
 	
 ?>
--- themes/default/index.template.php
+++ themes/default/index.template.php
@@ -1,12 +1,21 @@
 
+<?php	// TODO: implement login check and remove this php part
+	$visitor['loggedIn'] = false;
+	$visitor['name'] = 'foobar-user';
+?>
+
+<div id="topline">
 <div id="menu">
 	<ul>
 		<li><?php echo $txt['home']; ?></li>
 		<?php
-			if(true)	// TODO: check if logged in
-				echo '<li>'.$txt['login'].'</li>';
+			if($visitor['loggedIn'])
+				echo '<li>'.$txt['login'].'</li><li>'.$txt['register'].'</li>';
 			else
-				echo '<li>'.$txt['profile'].'</li><li>'.$txt['']
+				echo '<li>'.$txt['profile'].'</li><li>'.$txt['logout'].'</li>';
 		?>
 	</ul>
+</div>
+<?php if($visitor['loggedIn']) echo 'Welcome '.$visitor['name']; 
+		else echo 'Welcome. You may want to register and account for a mumble server or log in to change your details.'?>
 </div>
