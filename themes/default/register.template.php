bd007edf36280e55112d963c66411963794e58c3
Kissaki <kissaki@gmx.de>
2009-02-18 05:21:41 +0100

    more work on register

--- themes/default/register.template.php
+++ themes/default/register.template.php
@@ -3,7 +3,7 @@
 <?php
 	if(isset($_GET['action']) && $_GET['action']='doregister'){
 		//TODO: register user to server
-		
+		//TODO: Add optional mail activation
 		echo $txt['doregister_success'];
 	}else{
 ?>
@@ -12,13 +12,13 @@
 	<form action="./?section=register&amp;action=doregister" method="post">
 		<table>
 			<tr>
-				<td><?php echo $txt['username']; ?>:</td><td><input type="text" name="name" value="" /></td>
+				<td class="formitemname"><?php echo $txt['username']; ?>:</td><td><input type="text" name="name" value="" /></td><td class="helpicon"></td>
 			</tr><tr>
-				<td><?php echo $txt['email']; ?>:</td><td><input type="text" name="email" value="" /></td>
+				<td class="formitemname"><?php echo $txt['email']; ?>:</td><td><input type="text" name="email" value="" /></td><td class="helpicon"></td>
 			</tr><tr>
-				<td><?php echo $txt['password']; ?>:</td><td><input type="text" name="password" id="password" value="" /></td>
+				<td class="formitemname"><?php echo $txt['password']; ?>:</td><td><input type="text" name="password" id="password" value="" /></td><td class="helpicon"></td>
 			</tr><tr>
-				<td><?php echo $txt['password']; ?>:</td><td><input type="text" name="password2" id="password2" value="" /></td>
+				<td class="formitemname"><?php echo $txt['password_repeat']; ?>:</td><td><input type="text" name="password2" id="password2" value="" /></td><td class="helpicon"></td>
 			</tr>
 		</table>
 		<input type="submit" value="register" />
