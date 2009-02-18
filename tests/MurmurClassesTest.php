b166774a5fc7d1cc99a1aeeb6c5e6baa730a31c3
Kissaki <kissaki@gmx.de>
2009-02-18 03:43:21 +0100

    another try on testing, still not working

--- tests/MurmurClassesTest.php
+++ tests/MurmurClassesTest.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once('simpletest/autorun.php');
-require_once('../classes/MurmurClasses.php');
+//require_once('simpletest/autorun.php');
+require_once(dirname(__FILE__).'/../classes/MurmurClasses.php');
 
 class TestOfUserAccount extends UnitTestCase {
 	function testUserAccountObjectCreated() {
-        $obj = new UserRegistration();
-        $this->assertTrue($obj->returnTrue());
+        //$obj = new UserRegistration();
+        //$this->assertTrue($obj->returnTrue());
+        //$this->assertTrue(true,"Error");
+        $this->assertEqual(4,4,"das muss doch gehen");
     }
 	
 }
