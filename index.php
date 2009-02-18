e1c4cc55c167a1359fe91a99fd36ded0d0477cbd
Kissaki <kissaki@gmx.de>
2009-02-18 04:50:32 +0100

    menu links now link to pages correct templates are now loaded depending on _GET section

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 	require_once('settings.inc.php');
 	require_once('languages/'.$lang.'.php');
 ?>
+<?php	// TODO: implement login check and remove this php part
+	$visitor['loggedIn'] = false;
+	if(isset($_GET['loggedIn']) && $_GET['loggedIn'] == 'true') $visitor['loggedIn'] = true;
+	$visitor['name'] = 'foobar-user';
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -15,8 +20,27 @@
 	</script>-->
 </head>
 <body>
+
 <?php
-	
-	require_once($themedir.'/index.template.php');
+	if(isset($_GET['section']))
+		switch($_GET['section']){
+			case 'register':
+				$pageSection = 'register';
+				break;
+			
+			case 'login':
+				$pageSection = 'login';
+				break;
+			
+			case 'logout':
+				$pageSection = 'logout';
+				break;
+			
+			default:
+				$pageSection = 'index';
+		}
+	else
+		$pageSection = 'index';
+	require_once($themedir.'/'.$pageSection.'.template.php');
 ?>
 </body></html>
--- themes/default/header.template.php
+++ themes/default/header.template.php
@@ -1,16 +1,12 @@
-<?php	// TODO: implement login check and remove this php part
-	$visitor['loggedIn'] = false; if(isset($_GET['loggedIn'])) $visitor['loggedIn']=$_GET['loggedIn'];
-	$visitor['name'] = 'foobar-user';
-?>
 
 <div id="topline">
 <div id="menu">
 	<ul>
 		<li><a href="./" title="<?php echo $txt['home']; ?>"><?php echo $txt['home']; ?></a></li><?php
-			if($visitor['loggedIn'])
-				echo '<li><a href="./">'.$txt['login'].'</a></li><li><a href="./">'.$txt['register'].'</a></li>';
+			if(!$visitor['loggedIn'])
+				echo '<li><a href="./?section=login">'.$txt['login'].'</a></li><li><a href="./section=register">'.$txt['register'].'</a></li>';
 			else
-				echo '<li><a href="./">'.$txt['profile'].'</a></li><li><a href="./">'.$txt['logout'].'</a></li>';
+				echo '<li><a href="./section=profile">'.$txt['profile'].'</a></li><li><a href="./section=logout">'.$txt['logout'].'</a></li>';
 		?>
 	</ul>
 </div>
