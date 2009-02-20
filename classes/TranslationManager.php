95db1d86fe89726cb2d1d832278eb12bb6cf5bf8
Kissaki <kissaki@gmx.de>
2009-02-21 00:20:55 +0100

    first implemen

--- classes/TranslationManager.php
+++ classes/TranslationManager.php
@@ -1,5 +1,20 @@
 <?php
+/**
+ * The TranslationManager class provides an interface to get translated text.
+ * @author Jan Klass
+ */
 class TranslationManager {
-	
+	private static $instance;
+	public static function getInstance($obj=null){
+		if(!isset($instance))
+			if(isset($obj))
+				$this->instance = $obj;
+			else
+				$this->instance = new TranslationManager();
+		return $instance;
+	}
+	private function __construct(){
+//		SettingsManager::getInstance()->getLanguage();
+	}
 }
 ?>
