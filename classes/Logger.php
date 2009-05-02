4edbeb205495297d6f53cc2feb9e7f3956563dcc
Kissaki <kissaki@gmx.de>
2009-05-02 22:16:24 +0200

    logger now writes to a .log file, Fixed some exception catching

--- classes/Logger.php
+++ classes/Logger.php
@@ -11,10 +11,10 @@
  */
 class Logger{
 	public static function log_loginFail($uname,$pw){
-		DBManager::getInstance()->append('log_register', time().';'.$_SERVER['REMOTE_ADDR'].';'.$_SERVER['HTTP_REFERER'].';uname:'.$uname.';pw:'.$pw);
+		DBManager::getInstance()->append('log_register.log', time().';'.$_SERVER['REMOTE_ADDR'].';'.$_SERVER['HTTP_REFERER'].';uname:'.$uname.';pw:'.$pw);
 	}
 	public static function log_registration($uname){
-		DBManager::getInstance()->append('log_register', time().';'.$_SERVER['REMOTE_ADDR'].';'.$_SERVER['HTTP_REFERER'].';uname:'.$uname);
+		DBManager::getInstance()->append('log_register.log', time().';'.$_SERVER['REMOTE_ADDR'].';'.$_SERVER['HTTP_REFERER'].';uname:'.$uname);
 	}
 }
 
