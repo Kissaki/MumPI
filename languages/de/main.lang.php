8714151e3ee11447012fcade14d0847b2800f6d4
Kissaki <kissaki@gmx.de>
2009-07-29 12:42:42 +0200

    ger transl

--- languages/de/main.lang.php
+++ languages/de/main.lang.php
@@ -37,10 +37,8 @@
 	//Errors
 	$txt['error_unknowninterface']		= 'Falsche Konfiguration: unbekannter <acronym title="Datenbank">DB</acronym> Interface Typ!';
 	$txt['error_db_unknowntype']		= 'Der Datenbanktyp, den du spezifiziert hast (in der Einstellungs- "settings"-Datei) ist nicht verfügbar.';
-	//TODO de
-	$txt['error_noIceExtensionLoaded']	= 'It seems your PHP configuration is not running with the Ice extension.<br/>Please set up your PHP to load the Ice extension.<br/><br/>Ice is the middleware between <acronym title="Mumble Server">Murmur</acronym> and PHP/the Interface allowing PHP to call Murmur functions. Thus, this is necessary.';
-	//TODO de
-	$txt['error_noIceSliceLoaded']		= 'no slice';
+	$txt['error_noIceExtensionLoaded']	= 'Es scheint, als würde Ihr PHP ohne die geladene Ice Erweiterung laufen.<br/>Bitte konfigurieren Sie PHP so, dass es diese läd.<br/><br/>Ice ist das Bindeglied zwischen <acronym title="Mumble Server">Murmur</acronym> und PHP/dem Interface. Es erlaubt PHP Funktionen von Murmur aufzurufen. Daher wird es unbedingt benötigt.<br/>Für Informationen, wie man es konfiguriert, siehe <a href="http://mumble.sourceforge.net/Ice">mumble.sf.net/Ice</a>.';
+	$txt['error_noIceSliceLoaded']		= 'Es scheint Ihr PHP kennt die notwendige slice Datei nicht.<br/>Bitte geben Sie PHP über den slice.ice Konfigurationsparameter die Murmur.ice an.<br/>Für Informationen, wie man es konfiguriert, siehe <a href="http://mumble.sourceforge.net/Ice">mumble.sf.net/Ice</a>.';
 	$txt['error_noIce']					= 'Konnte keine Verbindung zu Ice herstellen.<br/>Entweder läuft dein Server nicht oder ohne Ice, weil er falsch eingestellt wurde. Prüfe deine Konfiguration.';
 	$txt['unknownserver']				= 'Kein solcher Server gefunden.';
 	$txt['error_missing_values']		= 'Es scheint, als seien nicht alle notwendigen Felder ausgefüllt/Werte definiert worden.';
