3d992b900aaf5a09018af281db584954ef374c7e
Kissaki <kissaki@gmx.de>
2009-08-22 15:44:42 +0200

    [security] removed server version from user section

--- themes/user/default/footer.template.php
+++ themes/user/default/footer.template.php
@@ -5,8 +5,6 @@
 </div>
 <div id="footer">
 	<?php
-		printf( tr('info_serverversion'), ServerInterface::getInstance()->getVersion() );
-		echo '<br/>';
 		printf( tr('info_scriptexecutiontime'), PHPStats::scriptExecTimeGet(), ceil(memory_get_peak_usage()/1000) );
 	?>
 </div>
