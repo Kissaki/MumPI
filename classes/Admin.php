a53267f0c6936d4890bd2754987a5604c7fd675b
Kissaki <kissaki@gmx.de>
2009-08-26 15:56:58 +0200

    created PermissionManager class created Admin class

---
+++ classes/Admin.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Admin Account
+ * TODO: use this class. Better than an array
+ */
+class Admin
+{
+	private $id;
+	private $name;
+	private $pw;
+	private $isGlobalAdmin;
+	
+	function __construct($id, $name, $pw, $isGlobalAdmin)
+	{
+		$this->id = intval($id);
+		$this->name = $name;
+		$this->pw = $pw;
+		$this->isGlobalAdmin = (bool)$isGlobalAdmin;
+	}
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	public function getName()
+	{
+		return $this->name;
+	}
+	
+	public function getPasswordHash()
+	{
+		return $this->pw;
+	}
+	
+	public function isGlobalAdmin()
+	{
+		return $isGlobalAdmin;
+	}
+}
+?>
---
+++ classes/PermissionManager.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Mumble PHP Interface by Kissaki
+ * Released under Creative Commons Attribution-Noncommercial License
+ * http://creativecommons.org/licenses/by-nc/3.0/
+ * @author Kissaki
+ */
+
+/**
+ * Permissionmanager for managing permissions,
+ * asking for permission…
+ */
+class PermissionManager
+{
+	private static $instance;
+	
+	/**
+	 * Get the object of the PermissionManager, specific to the section
+	 * @return object
+	 */
+	public static function getInstance()
+	{
+		if(self::$instance == null){
+			$secion = HelperFunctions::getActiveSection();
+			if (class_exists('PermissionManager_' . $section)) {
+				eval('self::$instance = new PermissionManager_' . $section . '();');
+			} else {
+				// TODO: transl
+				// TODO: errormsg
+				echo 'Unknown Permission Manager';
+			}
+		}
+		return self::$instance;
+	}
+	
+	
+}
+
+/**
+ * PermissionManager for admin section
+ */
+class PermissionManager_admin
+{
+	private $isGlobalAdmin;
+	
+	public function __construct()
+	{
+		if (SessionManager::getInstance()->isAdmin()) {
+			
+		} else {
+			$this->isGlobalAdmin = false;
+		}
+	}
+	
+	/**
+	 * Is global admin?
+	 * Can administrate all servers, add and remove virtual servers
+	 * @return bool
+	 */
+	public function isGlobalAdmin()
+	{
+		$admin = DBManager::getInstance()->getAdmin($_SESSION['adminLoggedInAs']);
+		if ($admin['isGlobalAdmin'] === true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Can start and stop servers?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanStartStop($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can edit the (virtual) servers (config) settings?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanEditConf($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can generate a new superuser password?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanGenSuUsPW($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can view registrations / accounts on the server?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanViewRegistrations($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can edit user accounts?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanEditRegistrations($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can create channels, Move users?
+	 * @return bool
+	 */
+	public function serverCanModerate($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can kick online users?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanKick($sid)
+	{
+		// TODO
+	}
+	
+	/**
+	 * Can ban users?
+	 * @param $sid
+	 * @return unknown_type
+	 */
+	public function serverCanBan($sid)
+	{
+		// TODO
+	}
+}
+
+?>
