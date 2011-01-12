22fac1d2d37a822bca301f714bdf43e9e51a7418
Kissaki <kissaki@gmx.de>
2011-01-12 17:04:58 +0100

    update description / info text

--- viewer2/example.php
+++ viewer2/example.php
@@ -2,17 +2,22 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
+  <style>
+    .code-placeholder { text-decoration:line-through; font-style:italic; }
+  </style>
 </head>
 <body>
 
-<h1>MumPI standalone JS <a href="http://mumble.sourceforge.net/Channel_Viewer_Protocol" rel="external">MumbleViewerProtocol</a> Viewer – valid HTML, no iframe</h1>
+<h1>MumPI standalone JS <a href="http://mumble.sourceforge.net/Channel_Viewer_Protocol" rel="external">MumbleViewerProtocol</a> Viewer</h1>
+<p><strong>valid HTML, no iframe – Object Oriented JavaScript</strong></p>
 
 <h2>How to embed:</h2>
 <pre>
 &lt;div id="mview-container">&lt;/div>
 &lt;script type="text/javascript" src="mview.js">&lt/script>
 &lt;script type="text/javascript">
-  mview.load(document.getElementById('mview-container'), '<span style="text-decoration:strike-through;">pathto</span>/MumPI/?view=json&amp;serverId=&amp;callback=?');
+  mv = new MView();
+  mv.load(document.getElementById('mview-container'), '<span class="code-placeholder">&lt;pathto&gt;</span>/MumPI/?view=json&amp;serverId=<span class="code-placeholder">&lt;ID&gt;</span>&amp;callback=?');
 &lt/script>
 </pre>
 
