00fea8e09bb420e7251f98fbe35b9b3e961c10bc
tkmorris <mauricioarozi@gmail.com>
2012-11-12 23:15:14 -0200

    Initial pt_BR translation.

---
+++ languages/pt_BR/user/profile.lang.php
@@ -0,0 +1,14 @@
+<?php
+	$txt['profile_head'] = 'Editar Perfil';
+	$txt['profile_note_texture'] = '<b>Nota:</b> Texturas devem ser 600x60 px. Você pode usar imagens tipo: png, jpeg, gif, raw (RGBA)';
+	$txt['profile_emptypassword'] = 'Campo de senha vazio. Ela não foi alterada.<br/>';
+
+	$txt['profile_removetexturefailed'] = 'Falha em remover textura.';
+	$txt['profile_texture_notempfile'] = 'Falha ao escolher textura. O arquivo temporário não existiu no servidor.';
+	$txt['profile_texture_imgresfail'] = 'Impossível criar recurso de imagem.';
+	$txt['profile_texture_wrongresolution'] = 'Tamanho da imagem não é 600x60.';
+	$txt['profile_texture_conversionfail'] = 'A conversão de imagem para cadeia de dados falhou.';
+	$txt['profile_texture_success'] = 'A textura foi enviada e configurada.<br/>';
+	$txt['profile_texture_fail'] = 'Falha ao configurar imagem.';
+	$txt['profile_texture_unknownext'] = 'Extensão de arquivo desconhecida.';
+	$txt['profile_texture_tmpopenfail'] = 'Falha ao abrir o arquivo temporário.';
